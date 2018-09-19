c5ee5334a82a0bb6abb1a6ebf7e42db50f6fc18f
Steven Torrence <storrence88@gmail.com>
2018-09-18 19:54:31 -0500

    Updated routes.rb and created local_env.yml

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  GmailAlerts::Application.routes.draw do
+    root to: 'sessions#new'
+    resources :sessions, only: :index
+    get "/auth/:provider/callback" => 'sessions#create'
+  end
 end
